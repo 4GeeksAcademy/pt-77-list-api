7ffc421e27eed1ece162489d3dbb3eb5748f9152
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-03-19 23:01:34 +0000

    fin

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	// grocery list
+	// that is going to contain a list of groceries 
+	// recieved from the user input
+	const [groceries, setGroceries] = useState([]);
+	const [userInput, setUserInput] = useState("");
+	
+	// item = dressing
+	const addToList = (e) => {
+		e.preventDefault();
+		let grocery = {value: userInput, bought: false}
+		
+		setGroceries([...groceries, grocery]);
+		// without spread operator [[apples", "bananas", "corn"], 'dressing']
+		// with spread operator ["apples", "bananas", "corn", "dressing"]
+		setUserInput("")
+	}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const updateBoughtItem = (index) => {
+		let groceryList = groceries
+		let thisGrocery = groceryList[index];
+		thisGrocery.bought = true;
+		setGroceries(groceryList);
+	}
+
+	const removeGrocery = (i) => {
+		const newArray = groceries.filter((grocery, index) => index !== i);
+		setGroceries(newArray);
+	  };
+
+	return (
+		<div className="text-center mt-5 h-100">
+            <input
+				type="text"
+				onChange={(e) => setUserInput(e.target.value)}
+				value={userInput}
+			/>
+			<button onClick={(e) => addToList(e)}>Add To List</button>
+
+			{/* we have an array of groceries and we want to display as a list on our page */}
+			<ul>
+								{/* item, index */}
+				{groceries?.map((grocery, index) => {		
+					if(grocery.bought != true) {
+						return (<li key={index}>{grocery.value} <span onClick={() => removeGrocery(index)}> ❌ </span></li>)
+					}			
+				})}
+			</ul>
 		</div>
 	);
 };
